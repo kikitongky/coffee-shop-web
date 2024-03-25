5cbed0ea8fd2f9db3be0241033c482aee99ed66f
kikitongky <tongkytongky07@gmail.com>
2024-03-26 02:35:28 +0700

    activate cart with alpine JS

--- js/main.js
+++ js/main.js
@@ -1,122 +1,135 @@
 // Show Menu
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle-menu'),
-      navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle-menu"),
+  navClose = document.getElementById("nav-close");
 
 /* Menu show */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
-
-
-
 
 /* Menu hidden */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
 /*===== Cart =====*/
 
-const cart = document.querySelector('.cart')
+const cart = document.querySelector(".cart");
 
-const cartToggle= document.querySelector('.nav__toggle-cart').addEventListener('click', () =>{
-    cart.classList.add('show-menu')
-})
+const cartToggle = document
+  .querySelector(".nav__toggle-cart")
+  .addEventListener("click", () => {
+    cart.classList.add("show-menu");
+  });
 
-const cartToggleLi= document.querySelector('.nav__toggle-cart-li').addEventListener('click', () =>{
-    cart.classList.add('show-menu')
-})
+const cartToggleLi = document
+  .querySelector(".nav__toggle-cart-li")
+  .addEventListener("click", () => {
+    cart.classList.add("show-menu");
+  });
 
-const cartClose = document.querySelector('.cart__close').addEventListener('click', () =>{
-    cart.classList.remove('show-menu')
-})
+const cartClose = document
+  .querySelector(".cart__close")
+  .addEventListener("click", () => {
+    cart.classList.remove("show-menu");
+  });
 
 /*===== REMOVE MENU MOBILE =====*/
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
-    const header = document.getElementById('header')
-    // Add a class if the bottom offset is greater than 50 of the viewport
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                       : header.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
-
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 // === SCROLL UP ===== \\
-const scrollUp = () =>{
-	const scrollUp = document.getElementById('scroll-up')
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						: scrollUp.classList.remove('show-scroll')
-}
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
 
-window.addEventListener('scroll', scrollUp);
-
+window.addEventListener("scroll", scrollUp);
 
 //=========  FILTER CARD by categori   ============\\
 
-const categoryLinks = document.querySelectorAll('.product__item');
+const categoryLinks = document.querySelectorAll(".product__item");
 
-function activeProduct(){
-    categoryLinks.forEach(link => link.classList.remove('active__product'));
-    this.classList.add('active__product');
+function activeProduct() {
+  categoryLinks.forEach((link) => link.classList.remove("active__product"));
+  this.classList.add("active__product");
 }
-function filtersCardByCategory(category){
-    const cards = document.querySelectorAll('.product__card');
+function filtersCardByCategory(category) {
+  const cards = document.querySelectorAll(".product__card");
 
-    cards.forEach(card => {
-        card.style.display = (category === 'all' || card.classList.contains(category)) ? 'block' : 'none';
-    })
-};
+  cards.forEach((card) => {
+    card.style.display =
+      category === "all" || card.classList.contains(category)
+        ? "block"
+        : "none";
+  });
+}
 
-categoryLinks.forEach(link => {
-    link.addEventListener('click', function(e){
-        e.preventDefault();
-        const selectedCategory = this.getAttribute('data-category');
-        activeProduct.call(this);
-        filtersCardByCategory(selectedCategory);
-    });
+categoryLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const selectedCategory = this.getAttribute("data-category");
+    activeProduct.call(this);
+    filtersCardByCategory(selectedCategory);
+  });
 });
-
-
 
 // ========= ACTIVE LINK ======
 
-const sections = document.querySelectorAll('section[id]')
-    
-const scrollActive = () =>{
-  	const scrollDown = window.scrollY
+const sections = document.querySelectorAll("section[id]");
 
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*="' + sectionId + '"]')
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
 
-		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                     
-	})
-    if (scrollDown + window.innerHeight >= document.body.offsetHeight - footer.offsetHeight){
-        document.querySelector('.nav__menu a[href="#footer"]').classList.add('active-link')
-    }else{
-        document.querySelector('.nav__menu a[href="#footer"]').classList.remove('active-link')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        '.nav__menu a[href*="' + sectionId + '"]'
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
     }
-}
-window.addEventListener('scroll', scrollActive);
+  });
+  if (
+    scrollDown + window.innerHeight >=
+    document.body.offsetHeight - footer.offsetHeight
+  ) {
+    document
+      .querySelector('.nav__menu a[href="#footer"]')
+      .classList.add("active-link");
+  } else {
+    document
+      .querySelector('.nav__menu a[href="#footer"]')
+      .classList.remove("active-link");
+  }
+};
+window.addEventListener("scroll", scrollActive);
--- src/app.js
+++ src/app.js
@@ -1,78 +1,146 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('products', () => ({
-        items: [
-            {
-                "id": 1,
-                "name": "Gayo Coffee",
-                "img": "asset/product1.jpg",
-                "category": "arabica",
-                "price": "Rp 100.000",
-                "description": "High-quality Arabica coffee from the highlands of Gayo, Aceh, known for its distinctive flavor and smooth aroma."
-              },
-              {
-                "id": 2,
-                "name": "Toraja Coffee",
-                "img": "asset/product2.jpg",
-                "category": "arabica",
-                "price": "Rp 120.000",
-                "description": "Premium Arabica coffee from South Sulawesi, known for its heavy and rich flavor with hints of chocolate and spices."
-              },
-              {
-                "id": 3,
-                "name": "Sidikalang Coffee",
-                "img": "asset/product3.jpg",
-                "category": "arabica",
-                "price": "Rp 110.000",
-                "description": "Quality Arabica coffee from Sidikalang, North Sumatra, with a gentle flavor profile and diverse aroma."
-              },
-              {
-                "id": 4,
-                "name": "Lampung Coffee",
-                "img": "asset/product4.jpg",
-                "category": "robusta",
-                "price": "Rp 80.000",
-                "description": "High-quality Robusta coffee from Lampung, Sumatra, with a strong flavor and low acidity, ideal for black coffee."
-              },
-              {
-                "id": 5,
-                "name": "Manggarai Coffee",
-                "img": "asset/product5.jpg",
-                "category": "robusta",
-                "price": "Rp 90.000",
-                "description": "Robusta coffee grown in the Manggarai region, Flores, known for its rich flavor and unique aroma."
-              },
-              {
-                "id": 6,
-                "name": "Bali Coffee",
-                "img": "asset/product6.jpg",
-                "category": "robusta",
-                "price": "Rp 90.000",
-                "description": "Robusta coffee from the island of Bali, known for its strong flavor and bold aroma, suitable for milk coffee."
-              },
-              {
-                "id": 7,
-                "name": "Sumatra Luwak Coffee",
-                "img": "asset/product7.jpg",
-                "category": "luwak",
-                "price": "Rp 500.000",
-                "description": "Authentic Kopi Luwak from Sumatra, produced through a unique process where coffee beans are fermented by civets, resulting in a delicate and smooth flavor."
-              },
-              {
-                "id": 8,
-                "name": "Java Luwak Coffee",
-                "img": "asset/product8.jpg",
-                "category": "luwak",
-                "price": "Rp 550.000",
-                "description": "Kopi Luwak from Java, with a rich and complex flavor profile and captivating aroma."
-              },
-              {
-                "id": 9,
-                "name": "Bali Luwak Coffee",
-                "img": "asset/product9.jpg",
-                "category": "luwak",
-                "price": "Rp 520.000",
-                "description": "Kopi Luwak produced in Bali, offering a smooth flavor with hints of caramel and chocolate."
-              }
-        ]
-    }));
-})
+document.addEventListener("alpine:init", () => {
+  Alpine.data("products", () => ({
+    items: [
+      {
+        id: 1,
+        name: "Gayo Coffee",
+        img: "asset/product1.jpg",
+        category: "arabica",
+        price: 100000,
+        description:
+          "High-quality Arabica coffee from the highlands of Gayo, Aceh, known for its distinctive flavor and smooth aroma.",
+      },
+      {
+        id: 2,
+        name: "Toraja Coffee",
+        img: "asset/product2.jpg",
+        category: "arabica",
+        price: 120000,
+        description:
+          "Premium Arabica coffee from South Sulawesi, known for its heavy and rich flavor with hints of chocolate and spices.",
+      },
+      {
+        id: 3,
+        name: "Sidikalang Coffee",
+        img: "asset/product3.jpg",
+        category: "arabica",
+        price: 110000,
+        description:
+          "Quality Arabica coffee from Sidikalang, North Sumatra, with a gentle flavor profile and diverse aroma.",
+      },
+      {
+        id: 4,
+        name: "Lampung Coffee",
+        img: "asset/product4.jpg",
+        category: "robusta",
+        price: 80000,
+        description:
+          "High-quality Robusta coffee from Lampung, Sumatra, with a strong flavor and low acidity, ideal for black coffee.",
+      },
+      {
+        id: 5,
+        name: "Manggarai Coffee",
+        img: "asset/product5.jpg",
+        category: "robusta",
+        price: 90000,
+        description:
+          "Robusta coffee grown in the Manggarai region, Flores, known for its rich flavor and unique aroma.",
+      },
+      {
+        id: 6,
+        name: "Bali Coffee",
+        img: "asset/product6.jpg",
+        category: "robusta",
+        price: 90000,
+        description:
+          "Robusta coffee from the island of Bali, known for its strong flavor and bold aroma, suitable for milk coffee.",
+      },
+      {
+        id: 7,
+        name: "Sumatra Luwak Coffee",
+        img: "asset/product7.jpg",
+        category: "luwak",
+        price: 500000,
+        description:
+          "Authentic Kopi Luwak from Sumatra, produced through a unique process where coffee beans are fermented by civets, resulting in a delicate and smooth flavor.",
+      },
+      {
+        id: 8,
+        name: "Java Luwak Coffee",
+        img: "asset/product8.jpg",
+        category: "luwak",
+        price: 550000,
+        description:
+          "Kopi Luwak from Java, with a rich and complex flavor profile and captivating aroma.",
+      },
+      {
+        id: 9,
+        name: "Bali Luwak Coffee",
+        img: "asset/product9.jpg",
+        category: "luwak",
+        price: 520000,
+        description:
+          "Kopi Luwak produced in Bali, offering a smooth flavor with hints of caramel and chocolate.",
+      },
+    ],
+  }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+
+    add(newItem) {
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        this.items = this.items.map((item) => {
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+
+      console.log(this.total);
+    },
+    remove(id) {
+      const cartItem = this.items.find((item) => item.id === id);
+
+      if (cartItem.quantity > 1) {
+        this.items = this.items.map((item) => {
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
+  });
+});
+
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
